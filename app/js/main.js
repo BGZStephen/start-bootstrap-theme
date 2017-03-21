4d3af6790b49e06baa617de60cd31736e65965e9
Stephen Wright <sjw948@gmail.com>
2017-03-21 11:49:35 +0000

    added lightbox feature to portfolio gallery

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 var navToggle = document.getElementsByClassName(`navbar__toggle`)[0];
 var navList = document.getElementsByClassName(`navbar__list`)[0];
+var lightbox = document.getElementsByClassName(`lightbox-container`)[0];
+var lightboxImage = document.getElementsByClassName(`lightbox-image`)[0];
+var overlay = document.getElementsByClassName(`portfolio-overlay`);
 
 navToggle.addEventListener(`click`, function() {
   console.log(`working`);
@@ -9,3 +12,24 @@ navToggle.addEventListener(`click`, function() {
     navList.style.opacity = 0;
   }
 })
+
+function showLightbox() {
+  var image = document.createElement(`img`)
+  image.src = this.nextElementSibling.src.replace(`thumbnails`, `fullsize`);
+  console.log(image);
+  lightboxImage.appendChild(image)
+  if(lightbox.style.display = "none") {
+    lightbox.style.display = "flex";
+  }
+}
+
+function hideLightbox() {
+  lightbox.style.display = "none";
+  lightboxImage.innerHTML = ""
+}
+
+for (let i = 0; i < overlay.length; i++) {
+  overlay[i].addEventListener(`click`, showLightbox)
+}
+
+lightbox.addEventListener(`click`, hideLightbox)
